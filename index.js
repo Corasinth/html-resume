1d1d45904cb57f0d938e8dc0f35bc023b61ca103
Sean Oh <seanoh@ucsb.edu>
2022-07-15 11:06:24 -0700

    add prompt questions

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+var questions = [
+  {
+    type: "input",
+    message: "What's your name?",
+    name: "name"
+  },
+  {
+    type: "input",
+    message: "What's your location?",
+    name: "location"
+  },
+  {
+    type: "input",
+    message: "What's your bio?",
+    name: "bio"
+  },
+  {
+    type: "input",
+    message: "What's your Linkedin URL?",
+    name: "linkedin"
+  },
+  {
+    type: "input",
+    message: "What's your Github URL?",
+    name: "github"
+  },
+  {
+    type: "input",
+    message: "What's your email?",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "What are your skills?",
+    name: "skills"
+  },
+  {
+    type: "input",
+    message: "What's your work experience?",
+    name: "experience"
+  },
+];
+
+async function promptUser() {
+  var response = await inquirer.prompt(questions);
+}
